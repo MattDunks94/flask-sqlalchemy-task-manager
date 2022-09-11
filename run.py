ec4efbd64877bc480470cbf394f5c80cbafd039d
Matt <mjd014@live.co.uk>
2022-09-11 14:43:56 +0000

    added code comments explaining the steps/cammands to follow to run file

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
+# pip3 install Flask-SQLAlchemy
+# pip3 install psycopg2
+# set_pg
+# psql
+# CREATE DATABASE taskmanager;
+# \c taskmanager
+# python3
+# from taskmanager import db
+# db.create_all() (enter)
+
 import os
 from taskmanager import app
 
