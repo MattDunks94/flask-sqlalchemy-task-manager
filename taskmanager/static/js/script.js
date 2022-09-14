4ad89776e81e09e7f017ab400297de8f5d16bec9
Matt <mjd014@live.co.uk>
2022-09-14 18:18:13 +0000

    added code comments.

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,3 +1,5 @@
+// All code, within the main function, is copied from materializecss.com
+
 document.addEventListener("DOMContentLoaded", function () {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
@@ -18,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
 
-    
+
     // collapsible initializataion
     let collapsibles = document.querySelectorAll(".collapsible");
     M.Collapsible.init(collapsibles);
