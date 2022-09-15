e37aea7757de3490161aa09ddb436c81c3ab30fc
Matt <mjd014@live.co.uk>
2022-09-15 11:14:24 +0000

    added code comments explaining what each function does

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -3,12 +3,14 @@ from taskmanager import app, db
 from taskmanager.models import Category, Task
 
 
+# Displays list of tasks created by user
 @app.route("/")
 def home():
     tasks = list(Task.query.order_by(Task.id).a())
     return render_template("tasks.html", tasks=tasks)
 
 
+# Displays the list of categories created by user
 @app.route("/categories")
 def categories():
     categories = list(Category.query.order_by(Category.category_name).all())
@@ -16,6 +18,7 @@ def categories():
 
 
 # This does not redirect back to categories route :( bug?
+# Is executed when the 'Add Category' button is clicked (categories.html)
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
@@ -26,6 +29,7 @@ def add_category():
     return render_template("add_category.html")
 
 
+# This function is executed when the 'Edit' button is clicked (categories.html)
 @app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
 def edit_category(category_id):
     # Get chosen category or display '404' error message
@@ -38,16 +42,18 @@ def edit_category(category_id):
 
 
 # Deletes chosen category, by user, and returns to categories.html
+# Is executed when the Delete button is clicked (categories.html)
 @app.route("/delete_category/<int:category_id>")
 def delete_category(category_id):
     # Get chosen category or display '404' error message
     category = Category.query.get_or_404(category_id)
     db.session.delete(category)
     db.session.commit()
-    # Redirect to categories route
+    # Redirect to categories route function
     return redirect(url_for("categories"))
 
 
+# This function is executed when the 'Add Task' button is clicked (tasks.html)
 @app.route("/add_task", methods=["GET", "POST"])
 def add_task():
     categories = list(Category.query.order_by(Category.category_name).all())
@@ -65,6 +71,7 @@ def add_task():
     return render_template("add_task.html", categories=categories)
 
 
+# This function is executed when the 'Edit' button is clicked (tasks.html)
 @app.route("/edit_task/<int:task_id>", methods=["GET", "POST"])
 def edit_task(task_id):
     task = Task.query.get_or_404(task_id)
@@ -78,7 +85,9 @@ def edit_task(task_id):
         db.session.commit()
     return render_template("edit_task.html", task=task, categories=categories)
 
-    @app.route("/delete_task/<int:task_id>")
+
+# This function is executed when the delete button is clicked
+@app.route("/delete_task/<int:task_id>")
 def delete_task(task_id):
     # Get chosen task or display '404' error message
     task = Task.query.get_or_404(task_id)
